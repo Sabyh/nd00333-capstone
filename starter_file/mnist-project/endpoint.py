7b4f5fbba571d76b06c9c62b7852555d2fdf410b
Sabyh11 <48311742+Sabyh11@users.noreply.github.com>
2021-01-03 21:42:27 +0500

    mnist project started

---
+++ starter_file/mnist-project/endpoint.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+# URL for the web service, should be similar to:
+# 'http://17fb482f-c5d8-4df7-b011-9b33e860c111.southcentralus.azurecontainer.io/score'
+scoring_uri = 'http://549f9b5f-fd26-4b63-b392-9d80ba3696c7.southcentralus.azurecontainer.io/score'
+
+# Two sets of data to score, so we get two results back
+data = {"data":
+        [
+            {
+                "id": 10,
+                "SepalLengthCm": 5.1,
+                "SepalWidthCm": 3.5,
+                "PetalLengthCm": 1.4,
+                "PetalWidthCm": 0.2
+            }
+        ]
+        }
+# Convert to JSON string
+input_data = json.dumps(data)
+with open("data.json", "w") as _f:
+    _f.write(input_data)
+
+# Set the content type
+headers = {'Content-Type': 'application/json'}
+
+# Make the request and display the response
+resp = requests.post(scoring_uri, input_data, headers=headers)
+print(resp.json())
